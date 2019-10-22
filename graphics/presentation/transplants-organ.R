faf21d4717dddb38513daf9e4f4abf184a5b771c
Ryan C. Thompson <rct@thompsonclan.org>
2019-10-22 10:12:07 -0700

    Reformat organ donation stats slide

--- graphics/presentation/transplants-organ.R
+++ graphics/presentation/transplants-organ.R
@@ -16,11 +16,11 @@ organ_data <- read.xlsx("transplants-organ.xlsx") %>%
 
 p <- ggplot(organ_data) + aes(x=Organ, y=Count, fill=Organ) +
     geom_col() +
-    geom_text(aes(label=str_c("  ", Organ, " (", Count, ")   "), hjust=ifelse(Count>max(Count)/4, 1, 0)), vjust=0.5) +
+    geom_text(aes(label=str_c("  ", Organ, " (", Count, ")   "), hjust=ifelse(Count>max(Count)/2, 1, 0)), vjust=0.5) +
     coord_flip() + scale_x_discrete(limits = rev(levels(organ_data$Organ))) +
     scale_y_continuous(expand=expand_scale(mult=c(0, 0.01))) +
     guides(fill=FALSE) +
     theme(axis.text.y=element_blank(),
           axis.ticks.y=element_blank()) +
     ylab("Transplants performed")
-ggprint(p, pdf("transplants-organ.pdf", width=6, height=4))
+ggprint(p, pdf("transplants-organ.pdf", width=6, height=3))
